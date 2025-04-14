61d0cdc3531fe4307e55ca49109975f7f252a6a7
cartonleg <momaniammar95@gmail.com>
2025-04-14 22:10:08 +0300

    created the process controller

---
+++ src/controllers/ProcessController.py
@@ -0,0 +1,41 @@
+from fastapi.responses import JSONResponse
+from ultralytics import YOLO
+from controllers.BaseController import BaseController
+from models.enums import ResponseEnums
+
+class ProcessController(BaseController):
+    def __init__(self):
+        super().__init__()
+        
+    def detect_pothole_in_image(self, image, weights, confidence:float = 0.2):
+        model = YOLO(weights)
+
+        results = model(image, conf=confidence)
+
+        if len(results[0].boxes) > 0:
+            detections = []
+            for box in results[0].boxes:
+                x1, y1, x2, y2 = box.xyxy[0].tolist()  
+                conf = box.conf.item()  
+                detections.append({
+                    "bounding_box": {"x1": x1, "y1": y1, "x2": x2, "y2": y2},
+                    "confidence": conf
+                })
+            return JSONResponse(content={
+                "status": "success",
+                "message": "Potholes detected",
+                "detections": detections,
+                "count": len(detections)
+            })
+        elif len(results[0].boxes) <= 0:
+            return JSONResponse(content={
+                "status": "success",
+                "message": "No potholes detected",
+                "detections": [],
+                "count": 0
+            })
+        return JSONResponse(content={
+            "status": "fail",
+            "message": ResponseEnums.IMAGE_PROCESS_FAIL.value})
+        
+        
---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+    IMAGE_PROCESS_FAIL = "failed while trying to process image"
