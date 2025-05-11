58b213733aa170f6fbebc979b6b07741071bb987
cartonleg <momaniammar95@gmail.com>
2025-05-11 18:29:37 +0300

    copy and pasted the parts in the json_return branch so it returns both json and images on command

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -7,6 +7,7 @@ from PIL import Image
 import tempfile
 import cv2
 import os
+from datetime import datetime
 
 class ProcessController(BaseController):
     def __init__(self):
@@ -14,7 +15,7 @@ class ProcessController(BaseController):
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.root_dir = os.path.dirname(self.current_dir)
         
-    def detect_pothole_in_image(self, image, weights, confidence:float = 0.2):
+    def detect_pothole_return_image(self, image, weights, confidence:float = 0.2):
         model = YOLO(weights)
 
         result = model(image, conf=confidence)
@@ -37,5 +38,37 @@ class ProcessController(BaseController):
 
         image_path = os.path.join(self.root_dir, 'assets', 'images', 'error_while_detecting.png')
         return image_path
-        
-        
+    
+
+    def detect_pothole_return_json(self, image, weights, confidence:float = 0.2):
+        model = YOLO(weights)
+
+        results = model(image, conf=confidence)
+
+        if len(results[0].boxes) > 0:
+            detections = []
+            for box in results[0].boxes:
+                x1, y1, x2, y2 = box.xyxy[0].tolist()  
+                conf = box.conf.item()  
+                detections.append({
+                    "bounding_box": {"x1": x1, "y1": y1, "x2": x2, "y2": y2},
+                    "confidence": conf
+                })
+            return {
+                "status": "success",
+                "message": "Potholes detected",
+                "detections": detections,
+                "count": len(detections),
+                "date-time": datetime.now()
+            }
+        elif len(results[0].boxes) <= 0:
+            return {
+                "status": "success",
+                "message": "No potholes detected",
+                "detections": [],
+                "count": 0,
+                "date-time": datetime.now()
+            }
+        return {
+            "status": "fail",
+            "message": ResponseEnums.IMAGE_PROCESS_FAIL.value}
--- src/routes/process.py
+++ src/routes/process.py
@@ -7,11 +7,12 @@ import os
 import shutil
 from zipfile import ZipFile
 import tempfile
+import uuid
 
 process_router = APIRouter(prefix='/process')
 
 @process_router.post('/image')
-async def upload_image(uploaded_images: List[UploadFile], 
+async def process_image_return_image(uploaded_images: List[UploadFile], 
                        app_settings:Settings = Depends(get_settings)):
     
 
@@ -31,7 +32,7 @@ async def upload_image(uploaded_images: List[UploadFile],
             with open(temp_filepath.name, "wb") as buffer:
                 shutil.copyfileobj(uploaded_image.file, buffer)
 
-            processed_image_path = ProcessController().detect_pothole_in_image(
+            processed_image_path = ProcessController().detect_pothole_return_image(
                 image=temp_filepath.name,
                 weights=model_path,
                 confidence=app_settings.CONFIDENCE
@@ -54,4 +55,40 @@ async def upload_image(uploaded_images: List[UploadFile],
     return FileResponse(path=zip_tmp.name,
                         media_type='application/zip',
                         filename='processed_images.zip')
+
+
+@process_router.post('/json')
+async def process_image_return_json(uploaded_images: List[UploadFile], 
+                       app_settings:Settings = Depends(get_settings)):
+    
+    output = {}
+    
+    for image in uploaded_images:
+    
+        file_ext = os.path.splitext(image.filename)[-1]
+        temp_filename = f"{uuid.uuid4()}{file_ext}"
+        temp_filepath = os.path.join("/tmp", temp_filename)
+        
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(current_dir, '..', 'assets', 'models', 'pothole_segmentation_model_YOLOv8.pt')
+        model_path = os.path.abspath(model_path)
+
+        try:
+            with open(temp_filepath, "wb") as buffer:
+                shutil.copyfileobj(image.file, buffer)
+
+            result = ProcessController().detect_pothole_return_json(
+                image=temp_filepath,
+                weights=model_path,
+                confidence=app_settings.CONFIDENCE
+            )
+            
+            output.update({image.filename: result})
+
+        finally:
+            if os.path.exists(temp_filepath):
+                os.remove(temp_filepath)
+    
+    return output
+
 
